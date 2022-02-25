1f8f81074282395a7132d295752f01e209e52b52
KatyRosli <96566968+KatyRosli@users.noreply.github.com>
2022-02-25 21:39:13 +0100

    added css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="container">
+      <div className="react-weather-container">
         <Weather />
         <footer>
           Open sourced by

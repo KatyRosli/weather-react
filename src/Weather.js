b95fb4dbce0896c450dcc092884572ada1fe8735
KatyRosli <96566968+KatyRosli@users.noreply.github.com>
2022-02-26 16:55:33 +0100

    css for weather.css

--- src/Weather.js
+++ src/Weather.js
@@ -4,46 +4,53 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <div className="container-fluid">
-        <form className="d-flex">
-          <input
-            className="form-control me-2"
-            type="search"
-            placeholder="Search for a city"
-            aria-label="Search"
-          />{" "}
-          or
-          <button type="button" className="btn btn-primary">
-            Locate my current location
-          </button>
-        </form>
-      </div>
+      <form className="form-inline">
+        <input
+          className="form-control"
+          type="search"
+          placeholder="Search for a city..."
+          autoFocus="on"
+          aria-label="Search"
+        />{" "}
+        <button type="button" className="btn btn-warning">
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </button>
+        <button type="button" className="btn btn-outline-warning">
+          <i className="fa-solid fa-location-dot"></i>
+        </button>
+      </form>
       <h1>Stockholm</h1>
+      <p>Wednesday 17:00</p>
+      <h2 className="Current temperature">
+        1<span className="celsius">°C</span>
+      </h2>
       <img
         src="https://basmilius.github.io/weather-icons/production/fill/all/clear-day.svg"
         alt="Weather icon"
-        className="Main-icon"
+        className="Weather-icon-current-day"
       ></img>
-      <h2>1°C</h2>
-      <h3>Mostly cloudy</h3>
-      <p>Last updated: Wed, 17:00</p>
+      <h3>Mostly sunny</h3>
       <div className="container-current">
         <div className="row">
           <div className="col-3">
             Wind
-            <span>2m/s</span>
+            <br />
+            <strong>2m/s</strong>
           </div>
           <div className="col-3">
             Humidity
-            <span>73%</span>
+            <br />
+            <strong>73%</strong>
           </div>
           <div className="col-3">
             Sunrise
-            <span>07:00</span>
+            <br />
+            <strong>07:00</strong>
           </div>
           <div className="col-3">
             Sunset
-            <span>18:00</span>
+            <br />
+            <strong>18:00</strong>
           </div>
         </div>
       </div>
@@ -52,27 +59,32 @@ export default function Weather() {
           <div className="col-2">
             Mon
             <img src="https://youthful-mirzakhani-0d8d97.netlify.app/images/png/01d.png"></img>
-            <span>1°C</span>
+            <br />
+            <strong>1°C</strong>
           </div>
           <div className="col-2">
             Tue
             <img src="https://youthful-mirzakhani-0d8d97.netlify.app/images/png/01d.png"></img>
-            <span>1°C</span>
+            <br />
+            <strong>1°C</strong>
           </div>
           <div className="col-2">
             Wed
             <img src="https://youthful-mirzakhani-0d8d97.netlify.app/images/png/01d.png"></img>
-            <span>1°C</span>
+            <br />
+            <strong>1°C</strong>
           </div>
           <div className="col-2">
             Thu
             <img src="https://youthful-mirzakhani-0d8d97.netlify.app/images/png/01d.png"></img>
-            <span>1°C</span>
+            <br />
+            <strong>1°C</strong>
           </div>
           <div className="col-2">
             Fri
             <img src="https://youthful-mirzakhani-0d8d97.netlify.app/images/png/01d.png"></img>
-            <span>1°C</span>
+            <br />
+            <strong>1°C</strong>
           </div>
         </div>
       </div>
